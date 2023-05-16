b301a80ae0f361b77a9adedf593f4fb8011dd671
katievlasic <kathrynvlasic@gmail.com>
2023-05-15 19:02:48 -0500

    add email regex and initial typeDefs commit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,28 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type User {
         _id: ID!
-        firstName: String!
+        userName: String!
+        password: String!
+        email: String!
+    }
+
+    type tradeItem{
+        _id: ID!
+        userID: String!
+        plantName: String!
+        traderID: String!
+    }
+
+    type tradeIP{
+        _id: ID!
+        userID: String!
+        plantName: String!
+    }
+
+    type wishList{
+        _id: ID!
+        userID: String!
+        wishName: String!
     }
     
     type Query {
