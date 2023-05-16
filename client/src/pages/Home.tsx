5d5cd2fa6c4dfb6f892fadd86996728dd8659950
SBars <samuelbarsuhn@gmail.com>
2023-05-16 16:04:18 -0500

    added framework for tiles. not styled yet

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Tile from "./Tile";
+
+const homeTiles = [
+    {
+        title: "put a title here",
+        image: "require (an image from the DB)",
+        description: "maybe a description?",
+        url: "route to the page",
+      },
+      {
+        title: "put a title here",
+        image: "require (an image from the DB)",
+        description: "maybe a description?",
+        url: "route to the page",
+      },
+]
 
 export default function Home() {
     return (
-        <div><h1>This is the Home Page</h1></div>
+        <div>
+            <input className="search" placeholder="Search for a plant"/>
+            {homeTiles.map((tile) => {
+                return (
+                  <div>
+                    <Tile
+                      title={tile.title}
+                      image={tile.image}
+                      description={tile.description}
+                      url={tile.url}
+                    ></Tile>
+                  </div>
+                
+            )
+            })}
+        </div>
     )
 }
---
+++ client/src/pages/Tile.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+interface cardProps {
+title: string,
+image: string,
+description: string,
+url: string
+}
+const Tile = (props: cardProps) => {
+  return (
+      <div className="tile-cont">
+        <div className="tile">
+          <div className="tile-title">{props.title}</div>
+          <img src={props.image} className="tile-image" />
+          <a href={props.url} className="tile-url">
+            <button className="site-but">More Info</button>
+          </a>
+          <div className="tile-desc">{props.description}</div>
+        </div>
+      </div>
+  );
+};
+
+export default Tile;
