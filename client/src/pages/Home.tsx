d47d88101f03baa3d437781e383d64f57dd47eca
SotoTheMayor <quickfire25@yahoo.com>
2023-05-15 19:04:52 -0500

    added profile page routing and file

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Home() {
     return (
-        <div><h1>Hi!</h1></div>
+        <div><h1>This is the Home Page</h1></div>
     )
 }
---
+++ client/src/pages/Profile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Profile() {
+    return (
+        <div><h1>This is the Profile Page</h1></div>
+    )
+}
