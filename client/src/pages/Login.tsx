38fe12d3fa4b670d90ff854c4b8deadc73ae3757
katievlasic <kathrynvlasic@gmail.com>
2023-05-20 09:36:06 -0500

    add states for login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN } from '../utils/mutations';
 
@@ -10,17 +10,16 @@ const Login = () => {
     const [formState, setFormState] = useState({ email: '', password: '' });
     const [login, { error, data }] = useMutation(LOGIN);
 
-    const handleChange = () => {
-    //   const { name, value } = event.target;
+    const handleChange = (event: any) => {
+      const { name, value } = event.target;
   
-    //   setFormState({
-    //     ...formState,
-    //     [name]: value,
-    //   });
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
     };
   
-    const handleFormSubmit = async () => {
-    //   event.preventDefault();
+    const handleFormSubmit = async (event: any) => {
       console.log(formState);
       try {
         const { data } = await login({
@@ -42,18 +41,13 @@ const Login = () => {
       <main className="flex-row justify-center mb-4">
         <div className="col-12 col-lg-10">
           <div className="card">
-            <h4 className="card-header bg-dark text-light p-2">Login</h4>
+            <h4 className="card-header bg-green-400 text-success p-2">Login</h4>
             <div className="card-body">
-              {data ? (
-                <p>
-                  Success! You may now head{' '}
-                  <Link to="/">back to the homepage.</Link>
-                </p>
-              ) : (
+            
                 <form onSubmit={handleFormSubmit}>
                   <input
                     className="form-input"
-                    placeholder="Your email"
+                    placeholder="email"
                     name="email"
                     type="email"
                     value={formState.email}
@@ -61,22 +55,21 @@ const Login = () => {
                   />
                   <input
                     className="form-input"
-                    placeholder="******"
+                    placeholder="password"
                     name="password"
                     type="password"
                     value={formState.password}
                     onChange={handleChange}
                   />
                   <button
-                    className="btn btn-block btn-primary"
+                    className="btn btn-block btn-info"
                     style={{ cursor: 'pointer' }}
                     type="submit"
                   >
                     Submit
                   </button>
                 </form>
-              )}
-  
+              
               {error && (
                 <div className="my-3 p-3 bg-danger text-white">
                   {error.message}
