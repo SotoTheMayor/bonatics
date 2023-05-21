c088b21d9abfac5fa00c9f83ffdce57afc3c2a14
SBars <samuelbarsuhn@gmail.com>
2023-05-21 16:01:13 -0500

    started on login page and made some changes to home and profile

--- client/src/pages/Tile.tsx
+++ client/src/pages/Tile.tsx
@@ -8,11 +8,11 @@ callback: any,
 const Tile = (props: cardProps) => {
   return (
       <div className="tile-cont">
+          <div onClick={props.callback} className="tile-url">
         <div className="tile">
           <div className="tile-title">{props.title}</div>
           <img src={props.image} className="tile-image" />
-          <div onClick={props.callback} className="tile-url">
-            <button className="site-but">More Info</button>
+            {/* <button className="site-but">More Info</button> */}
           </div>
           {/* <div className="tile-desc">{props.description}</div> */}
         </div>
